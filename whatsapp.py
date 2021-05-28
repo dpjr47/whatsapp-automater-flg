4be776b273a2898e80adf5bd2e3c3c310b73a6a8
Mohammed Aslam Nazar <53053624+dpjr47@users.noreply.github.com>
2021-05-29 02:25:05 +0530

    Update whatsapp.py
    #updates

--- whatsapp.py
+++ whatsapp.py
@@ -1,2 +1,5 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+917594066423', 'still on', 1, 47)
+pywhatkit.sendwhatmsg('enter phone number here with country code', 'Enter the message here', 1, 47)
+#Time is set in 24 hr format, so if you want to send a message at say 1pm, time should be given as 13, 00
+#install pywhatkit and pillow
+#make sure you have whatsapp web open on default browser
